858fd9231e711d13dd9c0a93a7d294d126c1b07e
Max-Hu <huzhiwise@gmail.com>
2015-11-21 17:28:38 +0800

    finish first

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,3 +1,20 @@
 function single_element(arr) {
-  
+  var evens = arr.filter(function(element,pos){
+    return pos % 2 != 0;})
+  var n = [];
+  var pos = 0;
+  var m = [];
+	for(var i = 0; i < evens.length; i++){
+		if (n.indexOf(evens[i]) == -1) {
+      n.push(evens[i]);
+    }
+    else {
+      m.push(n.indexOf(evens[i]) );
+    }
+	};
+  for (var i = 0; i < m.length; i++) {
+    n.splice(m[i],m[i]+1);
+  }
+	return n;
+
 }

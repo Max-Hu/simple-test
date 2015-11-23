640bf83700a4eefda3844c4a9a8415d548a3c15b
Max-Hu <huzhiwise@gmail.com>
2015-11-23 15:34:46 +0800

    js:solve the second

--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,3 +1,18 @@
 function calculate_median(arr) {
+  var evens = arr.filter(function(element,pos){
+    return pos % 2 != 0;})
+  if (evens.length %2 != 0) {
+    return getMiddle(evens);
+  }else {
+    var odds = arr.filter(function(element,pos){
+      return pos % 2 == 0;})
+    return getMiddle(odds)
+  }
+}
 
+function getMiddle(arr) {
+  var pos = parseInt(arr.length/2);
+  // var result = arr.splice(pos , pos + 1);
+   var result = arr.slice(pos , pos + 1);
+  return result.pop();
 }

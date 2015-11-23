43f442314749263b958ec34b0dee45e79fd5666c
Max-Hu <huzhiwise@gmail.com>
2015-11-23 15:34:01 +0800

    js:improve the function

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,20 +1,11 @@
 function single_element(arr) {
   var evens = arr.filter(function(element,pos){
     return pos % 2 != 0;})
-  var n = [];
-  var pos = 0;
-  var m = [];
-	for(var i = 0; i < evens.length; i++){
-		if (n.indexOf(evens[i]) == -1) {
-      n.push(evens[i]);
+  var result = [];
+  for(var i = 0; i < evens.length; i++){
+    if (evens.indexOf(evens[i]) == evens.lastIndexOf(evens[i])) {
+      result.push(evens[i]);
     }
-    else {
-      m.push(n.indexOf(evens[i]) );
-    }
-	};
-  for (var i = 0; i < m.length; i++) {
-    n.splice(m[i],m[i]+1);
   }
-	return n;
-
+	return result;
 }
